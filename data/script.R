43b69e95fb95a8566c19214e26a083f4aaf2f588
floquinho2024 <164389175+floquinho2024@users.noreply.github.com>
2024-05-14 20:47:15 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -3,3 +3,8 @@ library(readxl)
 lead <- read_excel("Lead.xlsx")
 saveRDS(lead, file = "data/lead_rds.rds")
 write.csv(lead, file = "data/lead.csv", row.names = FALSE)
+
+
+lead <- read_excel("Orçamentos.xlsx")
+saveRDS(lead, file = "data/orcamentos_rds.rds")
+write.csv(lead, file = "data/orcamentos.csv", row.names = FALSE)
