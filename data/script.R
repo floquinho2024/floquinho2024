3e5c705c843497b0dcdada9799e19c1527bb985a
floquinho2024 <164389175+floquinho2024@users.noreply.github.com>
2024-10-06 09:52:18 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -1,6 +1,8 @@
 library(readxl)
+library(dplyr)
 
 lead <- read_excel("Lead.xlsx")
+lead <- lead %>% filter(!is.na(Data))
 saveRDS(lead, file = "data/lead_rds.rds")
 write.csv(lead, file = "data/lead.csv", row.names = FALSE)
 
