a28cc2766d85956cc1d6de62a16e78da0884d6a3
floquinho2024 <164389175+floquinho2024@users.noreply.github.com>
2024-10-06 10:12:24 -0300

    Update script.R

--- data/script.R
+++ data/script.R
@@ -1,12 +1,35 @@
 library(readxl)
 library(dplyr)
+library(lubridate)
 
 lead <- read_excel("Lead.xlsx")
 lead <- lead %>% filter(!is.na(Data))
 saveRDS(lead, file = "data/lead_rds.rds")
 write.csv(lead, file = "data/lead.csv", row.names = FALSE)
 
+# Criar o resumo para Twilio
+ano_corrente <- max(year(lead$Data))
+ultima_atualizacao <- format(max(lead$Data), '%d/%m/%Y')
 
+total_leads_ano <- lead %>%
+  filter(year(Data) == ano_corrente) %>%
+  summarise(total_leads_ano = n()) %>%
+  pull(total_leads_ano)
+
+total_leads_mes <- lead %>%
+  filter(month(Data) == max(month(Data))) %>%
+  summarise(total_leads_mes = n()) %>%
+  pull(total_leads_mes)
+resumo <- paste("Leads",
+                sprintf("Total no ano: %d", total_leads_ano),
+                sprintf("Total no mês: %d", total_leads_mes),
+                sprintf("Última atualização: %s", ultima_atualizacao),
+                sep = "\n")
+
+writeLines(resumo, "data/resumo_leads.txt")
+
+
+## Orçamentos
 lead <- read_excel("Orçamentos.xlsx")
 saveRDS(lead, file = "data/orcamentos_rds.rds")
 write.csv(lead, file = "data/orcamentos.csv", row.names = FALSE)
